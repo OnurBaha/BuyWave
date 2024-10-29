bdfaab928d4fc75f30154b3164757b66e920bbb4
OnurBaha <koconurbaha@gmail.com>
2024-10-30 02:37:31 +0300

    Added ProductDetail and ProductImage Dtos

---
+++ Services/Catalog/BuyWave.Catalog/Dtos/ProductDetailDtos/GetByIdProductDetailDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BuyWave.Catalog.Dtos.ProductDetailDtos;
+
+public class GetByIdProductDetailDto
+{
+    public string ProductDetailID { get; set; }
+    public string ProductDescription { get; set; }
+    public string ProductInfo { get; set; }
+}
---
+++ Services/Catalog/BuyWave.Catalog/Dtos/ProductImageDtos/CreateProductImageDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BuyWave.Catalog.Dtos.ProductImageDtos;
+
+public class CreateProductImageDto
+{
+    public string ProductImageID { get; set; }
+    public string Image1 { get; set; }
+    public string Image2 { get; set; }
+    public string Image3 { get; set; }
+    public string ProductID { get; set; }
+}
