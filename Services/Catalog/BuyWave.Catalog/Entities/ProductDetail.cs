96592b997669a845e51f38edc13b60e804df4e59
OnurBaha <koconurbaha@gmail.com>
2024-10-30 02:39:51 +0300

    bugfix

--- Services/Catalog/BuyWave.Catalog/Entities/ProductDetail.cs
+++ Services/Catalog/BuyWave.Catalog/Entities/ProductDetail.cs
@@ -1,9 +1,17 @@
-﻿namespace BuyWave.Catalog.Entities;
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace BuyWave.Catalog.Entities;
 
 public class ProductDetail
 {
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
     public string ProductDetailID { get; set; }
     public string ProductDescription { get; set; }
     public string ProductInfo { get; set; }
+    public string ProductId { get; set; }
+    [BsonIgnore]
+    public Product Product { get; set; }
 
 }
--- Services/Catalog/BuyWave.Catalog/Entities/ProductImage.cs
+++ Services/Catalog/BuyWave.Catalog/Entities/ProductImage.cs
@@ -1,11 +1,17 @@
-﻿namespace BuyWave.Catalog.Entities;
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace BuyWave.Catalog.Entities;
 
 public class ProductImage
 {
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
     public string ProductImageID { get; set; }
     public string Image1 { get; set; }
     public string Image2 { get; set; }
     public string Image3 { get; set; }
-    public string ProductID { get; set; }
+    public string ProductId { get; set; }
+    [BsonIgnore]
     public Product Product { get; set; }
 }
