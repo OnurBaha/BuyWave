dd7bfdbd6888f0c6e470ff0be17948af7259504c
OnurBaha <koconurbaha@gmail.com>
2024-10-31 23:40:20 +0300

    Added Controllers

---
+++ Services/Catalog/BuyWave.Catalog/Controllers/ProductDetailsController.cs
@@ -0,0 +1,55 @@
+﻿using BuyWave.Catalog.Dtos.ProductDetailDtos;
+using BuyWave.Catalog.Services.ProductDetailServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuyWave.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductDetailsController : ControllerBase
+    {
+        private readonly IProductDetailService _productDetailService;
+
+        public ProductDetailsController(IProductDetailService productDetailService)
+        {
+            _productDetailService = productDetailService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ProductDetailList()
+        {
+            var values = await _productDetailService.GetAllProductDetailAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("id")]
+        public async Task<IActionResult> GetProductDetailById(string id)
+        {
+            var values = _productDetailService.GetByIdProductDetailAsync(id);
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProductDetail(CreateProductDetailDto createProductDetailDto)
+        {
+            await _productDetailService.CreateProductDetailAsync(createProductDetailDto);
+            return Ok("Ürün Detayları Başarıyla Eklendi");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteProductDetail(string id)
+        {
+            await _productDetailService.DeleteProductDetailAsync(id);
+            return Ok("Ürün Detayları Başarıyla Silindi");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateProductDetail(UpdateProductDetailDto updateProductDetailDto)
+        {
+            await _productDetailService.UpdateProductDetailAsync(updateProductDetailDto);
+            return Ok("Ürün Detayları Başarıyla Güncellendi");
+        }
+
+    }
+}
