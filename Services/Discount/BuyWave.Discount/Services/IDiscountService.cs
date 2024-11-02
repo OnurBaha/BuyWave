c41757f72fe3de1430f0796969d6eed979b139fd
OnurBaha <koconurbaha@gmail.com>
2024-11-02 18:31:44 +0300

    Added Discount Operations Services

--- Services/Discount/BuyWave.Discount/Services/IDiscountService.cs
+++ Services/Discount/BuyWave.Discount/Services/IDiscountService.cs
@@ -7,7 +7,7 @@ public interface IDiscountService
     Task <List<ResultCouponDto>> GetAllCouponAsync();
     Task CreateCouponAsync(CreateCouponDto createCouponDto);
     Task UpdateCouponAsync(UpdateCouponDto updateCouponDto);
-    Task DeleteCouponAsync(int couponId);
-    Task<GetByIdCouponDto> GetByIdCouponAsync(int couponId);
+    Task DeleteCouponAsync(int id);
+    Task<GetByIdCouponDto> GetByIdCouponAsync(int id);
 
 }
