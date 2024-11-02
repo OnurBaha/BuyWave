115df3ba866b8857dfaa50c176832155585040b8
OnurBaha <koconurbaha@gmail.com>
2024-11-02 17:21:42 +0300

    Discount Migration

--- Services/Discount/BuyWave.Discount/Context/DapperContext.cs
+++ Services/Discount/BuyWave.Discount/Context/DapperContext.cs
@@ -1,7 +1,27 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BuyWave.Discount.Entities;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
 
 namespace BuyWave.Discount.Context;
 
 public class DapperContext:DbContext
 {
+    private readonly IConfiguration _configuration;
+    private readonly string _connectionString;
+
+    public DapperContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        _connectionString = _configuration.GetConnectionString("DefaultConnection");
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;initial Catalog=BuyWaveDiscountDb;integrated Security=true");
+    }
+
+    public DbSet<Coupon> Coupons { get; set; }
+
+    public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
 }
